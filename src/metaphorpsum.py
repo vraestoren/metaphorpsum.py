12717ab89c6168f4973de81f4310f2e8d2f50c2b
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:08:32 +0300

    Update metaphorpsum.py

--- src/metaphorpsum.py
+++ src/metaphorpsum.py
@@ -1,27 +1,24 @@
-from requests import get
+from requests import Session
 
 class MetaphorpSum:
 	def __init__(self) -> None:
 		self.api = "http://metaphorpsum.com"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
-
 	def get_sentences(self, count: int) -> str:
-		return get(
-			f"{self.api}/sentences/{count}",
-			headers=self.headers).text
+		return self.session.get(
+			f"{self.api}/sentences/{count}").text
 
 	def get_paragraphs(self, count: int) -> str:
-		return get(
-			f"{self.api}/paragraphs/{count}",
-			headers=self.headers).text
+		return self.session.get(
+			f"{self.api}/paragraphs/{count}").text
 
 	def get_paragraphs_with_sentences(
 			self,
 			paragraphs_count: int,
 			sentences_count: int) -> str:
-		return get(
-			f"{self.api}/paragraphs/{paragraphs_count}/sentences_count",
-			headers=self.headers).text
+		return self.session.get(
+			f"{self.api}/paragraphs/{paragraphs_count}/sentences_count").text
